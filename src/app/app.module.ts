74690c4e704386361f7ed6a962c1b730c2de5409
Vishnu Vijayan <vvijayan@purdue.edu>
2018-02-05 15:19:24 -0500

    Create basic navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { NgModule } from '@angular/core';
 
 /* Angular Material Modules */
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatMenuModule, MatIconModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatButtonModule
+    FlexLayoutModule,
+    MatButtonModule, MatToolbarModule, MatMenuModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
