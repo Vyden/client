28cca756906fef67d247b45538152847137e078f
Vishnu Vijayan <vvijayan@purdue.edu>
2018-02-05 18:54:52 -0500

    Add sidenav to page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { breakpointsProvider, BreakpointsService, BreakpointEvent } from 'angular-breakpoints';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [breakpointsProvider()]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public sidenavMode: string
+
+  constructor(private _breakpointsService: BreakpointsService) {
+    this.sidenavMode = "side"
+
+    this._breakpointsService.changes.subscribe((bp: BreakpointEvent) => {
+      if (bp.name == "xs" || bp.name == "sm") {
+        this.sidenavMode = "over"
+      } else {
+        this.sidenavMode = "side"
+      }
+    });
+  }
 
   ngOnInit() {
   }
