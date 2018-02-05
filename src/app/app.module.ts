292789ecffd09a837d8d92a60aa6050e6e2cdec8
Vishnu Vijayan <vvijayan@purdue.edu>
2018-02-05 16:09:02 -0500

    Update navbar gradient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { UsernameComponent } from './components/username/username.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    UsernameComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsernameComponent } from '../username/username.component';
 
 @Component({
   selector: 'app-navbar',
