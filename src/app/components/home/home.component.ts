0f6b9ab3453bd9497878e711f95d9e21e7372c92
Vishnu Vijayan <vvijayan@purdue.edu>
2018-02-06 16:00:57 -0500

    Theme split color

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,6 +4,7 @@ import { breakpointsProvider, BreakpointsService, BreakpointEvent } from 'angula
 import { NavbarService } from '../../services/navbar/navbar.service';
 import { MatSidenav } from '@angular/material';
 import { CardBoilerplateComponent } from '../card-boilerplate/card-boilerplate.component';
+import { ThemeService } from '../../services/theme/theme.service';
 
 @Component({
   selector: 'app-home',
@@ -12,13 +13,17 @@ import { CardBoilerplateComponent } from '../card-boilerplate/card-boilerplate.c
   providers: [breakpointsProvider()]
 })
 export class HomeComponent implements OnInit {
-  
+
   @ViewChild('sidenav') sidenav: MatSidenav
   public sidenavMode: string
+  public themeClass: string
 
-  constructor(private _breakpointsService: BreakpointsService, private _navbarService: NavbarService) {
+  constructor(private _breakpointsService: BreakpointsService,
+    private _navbarService: NavbarService,
+    private _themeService: ThemeService) {
     this.sidenavMode = "side"
 
+    /* Listen for breakpoint changes */
     this._breakpointsService.changes.subscribe((bp: BreakpointEvent) => {
       if (bp.name == "xs" || bp.name == "sm") {
         this.sidenavMode = "over"
@@ -26,6 +31,12 @@ export class HomeComponent implements OnInit {
         this.sidenavMode = "side"
       }
     });
+
+    /* Listen for theme changes */
+    this._themeService.currentThemeClass
+      .subscribe((themeClass: string) => {
+        this.themeClass = themeClass
+      })
   }
 
   ngOnInit() {
