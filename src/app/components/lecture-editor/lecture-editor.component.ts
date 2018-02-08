2e6ff8baa3bdcf2273a6047057db13ddf9475ecb
Vishnu Vijayan <vvijayan@purdue.edu>
2018-02-07 23:17:24 -0500

    Create waves image

---
+++ src/app/components/lecture-editor/lecture-editor.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-lecture-editor',
+  templateUrl: './lecture-editor.component.html',
+  styleUrls: ['./lecture-editor.component.scss']
+})
+export class LectureEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
