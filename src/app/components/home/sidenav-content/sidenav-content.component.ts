a6195a1c16f5f8bce08bd8008961e61c2066b314
Matin Hormati <mthormati@gmail.com>
2018-02-10 20:59:07 -0500

    Add class buttons to sidenav

--- src/app/components/home/sidenav-content/sidenav-content.component.ts
+++ src/app/components/home/sidenav-content/sidenav-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from '../../../services/theme/theme.service'
 
 @Component({
   selector: 'app-sidenav-content',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavContentComponent implements OnInit {
 
-  constructor() { }
+  public themeClass: string = "default-theme";
+  public sampleClasses: string[] = ['CS 307: Software Enginerring', 'STAT 350: Introduction to Statistics of Dark Matter and Stuff', 'CS 252: Systems Programming'];
+
+  public themeColors: string[] = this._themeService.getThemes().map(themeArray => themeArray[1]);
+
+  constructor(private _themeService: ThemeService) { }
 
   ngOnInit() {
+		this._themeService.currentThemeClass.subscribe((theme: string) => {
+			this.themeClass = theme;
+		});
   }
 
+  public getButtonColor(index: number): string {
+    return this.themeColors[index % this.themeColors.length];
+  }
 }
