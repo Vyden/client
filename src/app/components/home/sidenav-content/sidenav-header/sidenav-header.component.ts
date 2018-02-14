14f736bb828d5005bde26065fe23d62087e071f3
Matin Hormati <mthormati@gmail.com>
2018-02-14 15:34:42 -0500

    Add animation to sidenav header

--- src/app/components/home/sidenav-content/sidenav-header/sidenav-header.component.ts
+++ src/app/components/home/sidenav-content/sidenav-header/sidenav-header.component.ts
@@ -1,10 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ThemeService } from '../../../../services/theme/theme.service';
+import { trigger, style, animate, transition, state} from '@angular/animations';
 
 @Component({
   selector: 'app-sidenav-header',
   templateUrl: './sidenav-header.component.html',
-  styleUrls: ['./sidenav-header.component.scss']
+  styleUrls: ['./sidenav-header.component.scss'],
+  animations: [
+    trigger('enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateX(100%)', opacity: 0}),
+          animate('250ms', style({transform: 'translateX(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)', opacity: 1}),
+          animate('250ms', style({transform: 'translateX(100%)', opacity: 0}))
+        ])
+      ],
+    ),
+  ],
 })
 export class SidenavHeaderComponent implements OnInit {
 
