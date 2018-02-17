75f21cdef7276c064d198eb6fd11596d3d548d04
Vishnu Vijayan <vvijayan@purdue.edu>
2018-02-17 14:06:46 -0500

    Create Lecture model

--- src/app/models/lecture.ts
+++ src/app/models/lecture.ts
@@ -1,3 +1,11 @@
+import { Course } from './course';
+import { TimelineItem } from './timelineItem';
+
 export class Lecture {
-    
+    public _id?: string;
+    public course: Course;
+    public title: string;
+    public timeline: TimelineItem [];
+    public date: Date;
+    public sky: string;
 }
