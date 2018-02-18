fedd1a78748497e36cf856e831b4282566e4c5d8
Vishnu Vijayan <vvijayan@purdue.edu>
2018-02-17 19:25:03 -0500

    Create timeline functions in service

--- src/app/services/lecture-editor/lecture-editor.service.ts
+++ src/app/services/lecture-editor/lecture-editor.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { TimelineItem } from '../../models/timelineItem';
 
 @Injectable()
 export class LectureEditorService {
 
-  constructor() { }
+  private timelineItems: TimelineItem []
+  private timelineItemsSource = new BehaviorSubject<TimelineItem []>([])
+  public currentTimelineItems = this.timelineItemsSource.asObservable()
+
+  constructor() {
+    this.currentTimelineItems
+      .subscribe((timelineItems: TimelineItem []) => {
+        this.timelineItems = timelineItems
+      })
+  }
+
+  public changeTimelineItems(timelineItems: TimelineItem []) {
+    this.timelineItemsSource.next(timelineItems)
+  }
+
+  public addTimelineItem(timelineItem: TimelineItem) {
+    this.timelineItems.push(timelineItem)
+    this.changeTimelineItems(this.timelineItems)
+  }
+
+  public clearTimelineItems() {
+    this.changeTimelineItems([])
+  }
 
 }
