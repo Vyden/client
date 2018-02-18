11a40f87d44c0f6cefabc5bb423b5e26d1811e08
Vishnu Vijayan <vvijayan@purdue.edu>
2018-02-17 19:40:40 -0500

    Start vertical stepper in timeline review

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { LectureEditorService } from '../../services/lecture-editor/lecture-editor.service';
+import { TimelineItem } from '../../models/timelineItem';
 
 @Component({
   selector: 'app-timeline',
@@ -9,9 +11,15 @@ export class TimelineComponent implements OnInit {
 
   @Input() lectureTime: number
 
-  constructor() { }
+  public timelineItems: TimelineItem []
+
+  constructor(private _lectureEditorService: LectureEditorService) { }
 
   ngOnInit() {
+    this._lectureEditorService.currentTimelineItems
+      .subscribe((items: TimelineItem []) => {
+        this.timelineItems = items
+      })
   }
 
 }
