1b5ed6059ef77d804554009f98a086a5ae8a822f
aungkaungmyat <amyat@purdue.edu>
2018-02-19 20:38:57 -0500

    linked to VR when play button is clicked

--- src/app/models/lecture.ts
+++ src/app/models/lecture.ts
@@ -7,18 +7,25 @@
 //     public sky: string;
 // }
 
-
-export enum ItemType {
-    MODEL = 'model',
-    VIDEO = 'video',
-    QUIZ = 'quiz'
-}
-
 export class Lecture {
     public id?: string;
-    public name: string;
-    public lecture: string;
-    public eventTime: number;
-    public type: ItemType;
-    public resource: string;
+    public answers: string [];
+    public correct: number;
+    public course: string;
+    public time: string;
 }
+
+// export enum ItemType {
+//     MODEL = 'model',
+//     VIDEO = 'video',
+//     QUIZ = 'quiz'
+// }
+
+// export class Lecture {
+//     public id?: string;
+//     public name: string;
+//     public lecture: string;
+//     public eventTime: number;
+//     public type: ItemType;
+//     public resource: string;
+// }
