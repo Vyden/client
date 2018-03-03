f37f31934eb557de36bf3edf0cb54d4e9257a6c7
Vishnu Vijayan <vvijayan@purdue.edu>
2018-03-03 17:06:09 -0500

    Create course settings panel

--- src/app/components/user-settings/user-settings.component.ts
+++ src/app/components/user-settings/user-settings.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 /* Models */
 import { UserInfo } from '../../models/userInfo';
+import { Course } from '../../models/course';
 
 /* Services */
 import { ClassesService } from '../../services/classes/classes.service';
@@ -18,10 +20,12 @@ export class UserSettingsComponent implements OnInit, OnDestroy {
   /* User data */
   public userInfo: UserInfo
   public authState: any
+  public myCourses: Course []
 
   constructor(private _authService: AuthService,
     private _classesService: ClassesService,
-    private _themeService: ThemeService) { }
+    private _themeService: ThemeService,
+    private _af: AngularFireDatabase) { }
 
   ngOnInit() {
     /* Subscribe to changes to the authstate */
@@ -36,6 +40,20 @@ export class UserSettingsComponent implements OnInit, OnDestroy {
     this._authService.currentUserInfo
       .subscribe((userInfo: UserInfo) => {
         this.userInfo = userInfo
+        console.log(this.userInfo);
+        /* Subscribe to course list */
+        if(this.userInfo && this.userInfo.courses) {
+          this._af.list("Courses/").valueChanges()
+            .subscribe((allCourses: Course []) => {
+              this.myCourses = []
+              allCourses.forEach((course: Course) => {
+                // Push to list if the user has this course
+                if(this.userInfo.courses[course.id]) {
+                  this.myCourses.push(course)
+                }
+              })
+            })
+        }
       })
 
     /* Change to indigo theme for this page */
