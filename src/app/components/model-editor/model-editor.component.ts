15955467457180b2e0df51b738fd38d9c05f3ee0
Vishnu Vijayan <vvijayan@purdue.edu>
2018-03-17 14:26:46 -0400

    Upload model to server

--- src/app/components/model-editor/model-editor.component.ts
+++ src/app/components/model-editor/model-editor.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DoneTickComponent } from '../done-tick/done-tick.component';
+import { v4 as uuid } from 'uuid';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
+import { UploadService } from '../../services/upload/upload.service';
 
 @Component({
   selector: 'app-model-editor',
@@ -17,7 +20,15 @@ export class ModelEditorComponent implements OnInit {
 
   public newModelMode: boolean
 
-  constructor() {
+  /* Dropzone data */
+  public showDropBox: boolean
+  public dropzoneActive: boolean
+  public videoActive: boolean
+  public showUploadProgress: boolean
+  public uploadProgress: number
+  public videoName: string
+
+  constructor(private _uploadService: UploadService) {
     this.modelMM = 0
     this.modelSS = 0
     this.modelStartTime = 0
@@ -43,6 +54,50 @@ export class ModelEditorComponent implements OnInit {
 
   public finishModel() {
     this.newModelMode = false
+  }
+
+  public onUpload($event: any) {
+    if ($event.srcElement.files.length) {
+      this.handleDrop($event.srcElement.files)
+    }
+  }
+
+  public handleDrop(fileList: FileList) {
+    // Create lecture object
+
+    this.uploadProgress = 0
+    this.videoActive = false
+    this.showUploadProgress = true
+    this.showDropBox = false
+    this.videoName = fileList[0].name
+
+    // Rename file
+    let blob = fileList[0].slice(0, -1, '.')
+    const videoFile: File = new File([blob], uuid(), { type: fileList[0].type })
+    console.log(videoFile.type);
+
+    console.log(videoFile)
+
+    this._uploadService.uploadModelFile(videoFile)
+      .subscribe((event: any) => {
+        if (event.type === HttpEventType.UploadProgress) {
+          // This is an upload progress event. Compute and show the % done:
+          const percentDone = Math.round(100 * event.loaded / event.total);
+          this.uploadProgress = percentDone
+          console.log(`File is ${percentDone}% uploaded.`);
+        } else if (event instanceof HttpResponse) {
+          this.showUploadProgress = false
+
+          console.log('File is completely uploaded!');
+
+          this.showDropBox = false
+          this.videoActive = true
+          let vid = document.createElement('video');
+          const fileURL = URL.createObjectURL(videoFile);
+          vid.src = fileURL;
+          // this._lectureEditorService.publishTimelineItem(videoItem)
+        }
+      })
   }
 
 }
--- src/app/services/upload/upload.service.ts
+++ src/app/services/upload/upload.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpRequest, HttpHeaders, HttpParams, HttpErrorResponse, Ht
 @Injectable()
 export class UploadService {
 
-  private uploadUrl: string = 'http://indy.science/uploadVideo'
+  private uploadUrl: string = 'http://indy.science/'
   // private uploadUrl: string = 'http://localhost:5000/upload'
 
   constructor(private _httpClient: HttpClient) { }
@@ -23,7 +23,26 @@ export class UploadService {
       // withCredentials: true,
     }
 
-    const req = new HttpRequest('POST', this.uploadUrl, formData, options);
+    const req = new HttpRequest('POST', this.uploadUrl + 'uploadVideo', formData, options);
+
+    return this._httpClient.request(req)
+  }
+
+  public uploadModelFile(modelFile: File) {
+    console.log(modelFile);
+    let params = new HttpParams();
+
+    let formData = new FormData();
+    formData.append('upload', modelFile)
+
+    const options = {
+      // headers: new HttpHeaders().set('Authorization', this.loopBackAuth.accessTokenId),
+      params: params,
+      reportProgress: true,
+      // withCredentials: true,
+    }
+
+    const req = new HttpRequest('POST', 'http://localhost:3001/uploadModel', formData, options);
 
     return this._httpClient.request(req)
   }
