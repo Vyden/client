7ed854ab5031d2ca64b8f221d8728e6d92f89d31
Matin Hormati <mthormati@gmail.com>
2018-03-18 14:58:44 -0400

    Create student and instructor quiz panels.

---
+++ src/app/components/quiz-panel/quiz-instructor/quiz-instructor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-instructor',
+  templateUrl: './quiz-instructor.component.html',
+  styleUrls: ['./quiz-instructor.component.scss']
+})
+export class QuizInstructorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/quiz-panel/quiz-panel.component.ts
+++ src/app/components/quiz-panel/quiz-panel.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { ClassesService } from '../../services/classes/classes.service'
+import { UserInfo } from '../../models/userInfo';
+import { Course } from '../../models/course';
 
 @Component({
   selector: 'app-quiz-panel',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizPanelComponent implements OnInit {
 
-  constructor() { }
+  public userInfo: UserInfo;
+  public activeCourse: Course;
+
+  constructor(private _authService: AuthService,
+                    private _classService: ClassesService) { }
 
   ngOnInit() {
+    /* Subscribe to user info */
+    this._authService.currentUserInfo
+      .subscribe((userInfo: UserInfo) => {
+        this.userInfo = userInfo;
+      })
+
+    this._classService.activeCourse
+      .subscribe((course) => {
+        this.activeCourse = course;
+      })
+  }
+
+  public isClassInstructor(): boolean {
+    if (this.userInfo && this.activeCourse) {
+      if (this.userInfo.UID === this.activeCourse.instructor) return true;
+    }
+    return false;
   }
 
 }
