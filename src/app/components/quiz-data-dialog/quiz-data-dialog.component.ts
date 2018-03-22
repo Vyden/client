13ad3a38bad6c87f3d60bdbc61076b183ac11b4d
Vishnu Vijayan <vvijayan@purdue.edu>
2018-03-22 16:16:05 -0400

    Install handsontable

--- src/app/components/quiz-data-dialog/quiz-data-dialog.component.ts
+++ src/app/components/quiz-data-dialog/quiz-data-dialog.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
 import { AngularFireDatabase } from 'angularfire2/database';
 
+/* Models */
+import { QuizResponse } from '../../models/quizResponse';
+
 @Component({
   selector: 'app-quiz-data-dialog',
   templateUrl: './quiz-data-dialog.component.html',
@@ -11,6 +14,7 @@ export class QuizDataDialogComponent implements OnInit {
 
   public courseId: string
   public lectureId: string
+  public hotData: any
 
   constructor(private _dialogRef: MatDialogRef<QuizDataDialogComponent>, private _af: AngularFireDatabase) { }
 
@@ -21,8 +25,9 @@ export class QuizDataDialogComponent implements OnInit {
   private getLectureQuizResponses() {
     this._af.list(`Courses/${this.courseId}/lectureQuizResponses/${this.lectureId}`)
       .valueChanges()
-      .subscribe((res: any) => {
-        console.log(res);
+      .subscribe((quizResponses: QuizResponse []) => {
+        console.log(quizResponses);
+        this.hotData = quizResponses
       })
   }
 
--- src/app/modules/dialogs/dialogs.module.ts
+++ src/app/modules/dialogs/dialogs.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HotTableModule } from '@handsontable/angular';
 
 /* Angular Material Modules */
 import {
@@ -17,6 +19,8 @@ import { QuizDataDialogComponent } from '../../components/quiz-data-dialog/quiz-
   imports: [
     CommonModule,
     FormsModule,
+    BrowserModule,
+    HotTableModule,
     MatButtonModule, MatToolbarModule, MatMenuModule, MatIconModule, MatSidenavModule, MatCardModule,
     MatInputModule, MatCheckboxModule, MatStepperModule, MatSliderModule, MatRadioModule, MatListModule,
     MatSlideToggleModule, MatDialogModule, MatProgressSpinnerModule, MatProgressBarModule
