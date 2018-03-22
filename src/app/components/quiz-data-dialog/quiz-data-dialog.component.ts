d7834c880281a767e18c539122e86d53794d83e0
Vishnu Vijayan <vvijayan@purdue.edu>
2018-03-22 16:03:55 -0400

    Get lectureQuizResponses from firebase

--- src/app/components/quiz-data-dialog/quiz-data-dialog.component.ts
+++ src/app/components/quiz-data-dialog/quiz-data-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-quiz-data-dialog',
@@ -11,9 +12,18 @@ export class QuizDataDialogComponent implements OnInit {
   public courseId: string
   public lectureId: string
 
-  constructor(private _dialogRef: MatDialogRef<QuizDataDialogComponent>) { }
+  constructor(private _dialogRef: MatDialogRef<QuizDataDialogComponent>, private _af: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.getLectureQuizResponses()
+  }
+
+  private getLectureQuizResponses() {
+    this._af.list(`Courses/${this.courseId}/lectureQuizResponses/${this.lectureId}`)
+      .valueChanges()
+      .subscribe((res: any) => {
+        console.log(res);
+      })
   }
 
 }
