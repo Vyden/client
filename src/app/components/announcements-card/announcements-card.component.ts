f33fbc197a1f368006b6fbf7dcce07202440e565
aungkaungmyat <amyat@purdue.edu>
2018-03-22 23:14:52 -0400

    Showed pinned post on the top of announcements

--- src/app/components/announcements-card/announcements-card.component.ts
+++ src/app/components/announcements-card/announcements-card.component.ts
@@ -23,6 +23,7 @@ export class AnnouncementsCardComponent implements OnInit {
 
   public announcements: Observable<Announcement []>
   public announcementList : Announcement[] = [];
+  public arrangedAnnouncements: Announcement[] = [];
 
   constructor(private _lecturesService: LecturesService,
     private _authService: AuthService,
@@ -54,6 +55,15 @@ export class AnnouncementsCardComponent implements OnInit {
         this.announcements.subscribe(res => {
           this.announcementList = res;
           console.log(this.announcementList);
+          for(var i = this.announcementList.length-1 ; i >= 0  ; i--){
+            if(this.announcementList[i].pinned === true){
+              this.arrangedAnnouncements.push(this.announcementList[i]);
+              this.announcementList.splice(i,1);
+            }
+          }
+          for(var i = this.announcementList.length-1 ; i >= 0  ; i--){
+            this.arrangedAnnouncements.push(this.announcementList[i]);
+          }
         })
         // this.lectures = this._lecturesService.getFirebaseLectures()
         // this.announcements = this.
