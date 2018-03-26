31ebcb1174e7114ad7969da191bca88e127b51b3
aungkaungmyat <amyat@purdue.edu>
2018-03-26 01:59:54 -0400

    Got graph to display

--- src/app/components/quiz-panel/quiz-instructor/quiz-instructor.component.ts
+++ src/app/components/quiz-panel/quiz-instructor/quiz-instructor.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { ClassesService } from '../../../services/classes/classes.service';
+import { Observable } from 'rxjs/Observable';
+import { Course } from '../../../models/course';
+// import { Lecture } from '../../../models/lecture';
 
 @Component({
   selector: 'app-quiz-instructor',
@@ -7,9 +14,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizInstructorComponent implements OnInit {
 
-  constructor() { }
+  public currentCourse: Course;
+
+  // data pulled from firebase
+   single = [
+    {
+      "name": "a",
+      "value": 50
+    },
+    {
+      "name": "b",
+      "value": 87
+    },
+    {
+      "name": "c",
+      "value": 67
+    },
+    {
+      "name": "d",
+      "value": 100
+    }
+  ];
+
+  // changing the size of graph
+  view: any[] = [500, 200];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Answer Choice';
+  showYAxisLabel = true;
+  yAxisLabel = 'Number of Students';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor(private _firebase: AngularFireDatabase,
+    private _classesService: ClassesService) { }
 
   ngOnInit() {
+    this._classesService.activeCourse
+      .subscribe((currentCourse: Course) => {
+        this.currentCourse = currentCourse
+        
+      })
+  }
+
+  onSelect(event) {
+    console.log(event);
   }
 
 }
