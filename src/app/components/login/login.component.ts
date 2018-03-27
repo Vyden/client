82583a05ebfc5d59673f9baf6eca1d8fcb4334f3
Vishnu Vijayan <vvijayan@purdue.edu>
2018-03-26 21:49:18 -0400

    Implement logins with new page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+/* Services */
+import { AuthService } from '../../services/auth/auth.service';
+
+/* Models */
+import { UserInfo } from '../../models/userInfo';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public email: string
+  public password: string
+  public fullName: string
+  public isInstructor: boolean
+  public authData: any
+
+  constructor(private _authService: AuthService, private _router: Router) { }
 
   ngOnInit() {
+    this._authService.currentUserObservable
+      .subscribe(user => console.log(user))
+  }
+
+  public createUser() {
+    const userInfo: UserInfo = {
+      fullName: this.fullName,
+      courses: [],
+      isInstructor: !!this.isInstructor
+    }
+
+    this._authService.createUser(this.email, this.password, userInfo)
+      .then(data => console.log(data))
+      .catch(err => alert(err))
+  }
+
+  public loginUser() {
+    this._authService.loginUserEmail(this.email, this.password)
   }
 
 }
