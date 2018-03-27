71f9be53dddd031d82a7ff264f44e41d7c84dcb8
aungkaungmyat <amyat@purdue.edu>
2018-03-26 21:59:47 -0400

    Change with play icon with happy_play button

--- src/app/models/course.ts
+++ src/app/models/course.ts
@@ -1,5 +1,4 @@
 export class Course {
-<<<<<<< HEAD
     public id?: string;
     public instructor: string;
     public title: string;
@@ -10,16 +9,4 @@ export class Course {
     public lectureQuizResponses: string [];
     public mean?: number;
     public sd?: number;
-=======
-  public id?: string;
-  public mean: string;
-  public sd: string;
-  public instructor: string;
-  public title: string;
-  public students: string [];
-  public lectures: string [];
-  public announcements?: string [];
-  public studentQuizResponses: string [];
-  public lectureQuizResponses: string [];
->>>>>>> 6db26ef8cf05b46887ef2f789b5ab42e5952fcc8
 }
