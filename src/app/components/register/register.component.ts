0c9d1126de6ce3fc54dd07cad64168c514d63d63
Vishnu Vijayan <vvijayan@purdue.edu>
2018-03-26 22:07:08 -0400

    Implement register functionality in new page

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { UserInfo } from '../../models/userInfo';
+import { User } from '@firebase/auth-types';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public email: string
+  public password: string
+  public fullName: string
+  public isInstructor: boolean
+  public authData: any
+  public user: User
+
+  constructor(private _authService: AuthService) { }
 
   ngOnInit() {
+    this._authService.currentUserObservable
+      .subscribe(user => this.user = user)
+  }
+
+  public createUser() {
+    const userInfo: UserInfo = {
+      fullName: this.fullName,
+      courses: [],
+      isInstructor: !!this.isInstructor
+    }
+
+    this._authService.createUser(this.email, this.password, userInfo)
+      .then(data => {
+        console.log(data)
+        this._authService.loginUserEmail(this.email, this.password)
+      })
+      .catch(err => alert(err))
   }
 
 }
