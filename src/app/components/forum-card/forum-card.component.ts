1340187ba8205d647ff69caad837f6f14cd1a010
Vishnu Vijayan <vvijayan@purdue.edu>
2018-04-07 21:08:53 -0400

    Create function to build new question

--- src/app/components/forum-card/forum-card.component.ts
+++ src/app/components/forum-card/forum-card.component.ts
@@ -2,8 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 /* Services */
 import { ThemeService } from '../../services/theme/theme.service';
+import { ClassesService } from '../../services/classes/classes.service';
+import { AuthService } from '../../services/auth/auth.service';
 
 /* Models */
+import { Course } from '../../models/course';
+import { UserInfo } from '../../models/userInfo';
+import { ForumQuestion, ForumAnswer } from '../../models/forum';
 
 @Component({
   selector: 'app-forum-card',
@@ -12,9 +17,21 @@ import { ThemeService } from '../../services/theme/theme.service';
 })
 export class ForumCardComponent implements OnInit {
 
+  /* Theme data */
   public themeClass: string
 
-  constructor(private _themeService: ThemeService) { }
+  /* User data */
+  private userInfo: UserInfo
+
+  /* Course data */
+  public currentCourse: Course
+
+  /* Forum data */
+  public buildQuestion: ForumQuestion
+
+  constructor(private _themeService: ThemeService,
+    private _classesService: ClassesService,
+    private _authService: AuthService) { }
 
   ngOnInit() {
     /* Listen for changes to theme */
@@ -22,6 +39,29 @@ export class ForumCardComponent implements OnInit {
       .subscribe((themeClass: string) => {
         this.themeClass = themeClass
       })
+
+    /* Listen for changes to course */
+    this._classesService.activeCourse
+      .subscribe((currentCourse: Course) => {
+        this.currentCourse = currentCourse
+      })
+
+    /* Subscribe to user info */
+    this._authService.currentUserInfo
+      .subscribe((userInfo: UserInfo) => {
+        this.userInfo = userInfo
+      })
+  }
+
+  public createQuestion(isNote: boolean = false) {
+    // Initialize new question
+    let newQuestion = new ForumQuestion()
+    newQuestion.author = this.userInfo.UID
+    newQuestion.dateCreated = Date.now()
+    newQuestion.text = null
+    newQuestion.isNote = isNote
+
+    this.buildQuestion = newQuestion
   }
 
 }
